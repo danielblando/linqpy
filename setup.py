1dd616208773c1b75044e8b12ec72d29816528cc
Daniel Blando <daniel@blando.com.br>
2015-10-08 20:03:27 -0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Daniel'
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='linqpy',
+    version='0.0.1',
+    url='https://github.com/danielblando/linqpy',
+    license='MIT',
+    description='Simple implementation of .NET Linq in python list.',
+    author=u'Daniel Blando',
+    author_email='daniel@blando.com.br',
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet",
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+    ],
+)
---
+++ src/linqpy.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Daniel'
+
+
+class List(list):
+
+    def all(self, func):
+        return len([x for x in self if func(x)]) == len(self)
+
+    def any(self, func=None):
+        if not func:
+            return len(self) > 0
+        return self.first_or_default(func) is not None
+
+    def element_at(self, index):
+        return self[index]
+
+    def element_at_or_default(self, index):
+        if len(self) > index:
+            return None
+        return self.element_at(index)
+
+    def first(self, func=None):
+        if not func:
+            return self.element_at(0)
+        return next((x for x in self if func(x)))
+
+    def first_or_default(self, func=None):
+        if not func:
+            return self.element_at_or_default(0)
+        return next((x for x in self if func(x)), None)
