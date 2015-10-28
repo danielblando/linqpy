75404273519d2be64b7a84ae5428e1e7bd789696
Daniel Blando <daniel@blando.com.br>
2015-10-28 18:16:28 -0200

    completing linq library

--- src/linqpy/list.py
+++ src/linqpy/list.py
@@ -16,7 +16,7 @@ class List(list):
         return self[index]
 
     def element_at_or_default(self, index):
-        if len(self) < index:
+        if index < 0 or index >= len(self):
             return None
         return self.element_at(index)
 
@@ -30,6 +30,33 @@ class List(list):
             return self.element_at_or_default(0)
         return next((x for x in self if func(x)), None)
 
+    def last(self, func=None):
+        if not func:
+            return self.element_at(len(self)-1)
+        l = self.where(func)
+        return l.element_at(len(l)-1)
+
+    def last_or_default(self, func=None):
+        if not func:
+            return self.element_at_or_default(len(self)-1)
+        l = self.where(func)
+        return l.element_at_or_default(len(l)-1)
+
     def new_list(self):
         return List(self)
+
+    def single(self, func=None):
+        l = self.where(func) if func else self
+        if len(l) != 1:
+            raise Exception("None or more than one element at the list")
+        return l.element_at(0)
+
+    def single_or_default(self, func=None):
+        l = self.where(func) if func else self
+        if len(l) != 1:
+             return None
+        return l.element_at(0)
+
+    def where(self, func):
+        return List([x for x in self if func(x)])
 
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,11 +2,17 @@
 __author__ = 'Daniel'
 
 
-from unittest import TestCase, main
+import os, sys
+sys.path.insert(0, os.path.abspath(__file__+"/../../src"))
 
-from linqpy.linqpy import List
+from unittest import TestCase, main
+from linqpy.list import List
 
 a = List([1, 2, 3])
+b = List()
+c = List([1])
+d = List([{"id": 1, "name": "bla"}, {"id": 1, "name": "ble"}])
+
 
 class Tests(TestCase):
 
@@ -35,10 +41,43 @@ class Tests(TestCase):
         self.assertEqual(a.first_or_default(lambda x: x == 2), 2)
         self.assertEqual(a.first_or_default(lambda x: x == 5), None)
 
-    def test_new_list(self):
-        b = a.new_list()
-        self.assertNotEqual(id(a), id(b), "New list not returning different obj")
+    def test_last(self):
+        self.assertEqual(a.last(), 3)
+        self.assertRaises(IndexError, b.last)
+        self.assertEqual(a.last(lambda x: x == 2), 2)
+        self.assertRaises(IndexError, a.last, lambda x: x > 5)
+        self.assertEqual(d.last(lambda x: x["id"] == 1)["name"], "ble")
 
+    def test_last_or_default(self):
+        self.assertEqual(a.last_or_default(), 3)
+        self.assertEqual(b.last_or_default(), None)
+        self.assertEqual(a.last_or_default(lambda x: x == 2), 2)
+        self.assertEqual(a.last_or_default(lambda x: x > 5), None)
+        self.assertEqual(d.last(lambda x: x["id"] == 1)["name"], "ble")
+
+    def test_new_list(self):
+        __new_list = a.new_list()
+        self.assertNotEqual(id(a), id(__new_list), "New list not returning different obj")
+
+    def test_single(self):
+        self.assertRaises(Exception, a.single)
+        self.assertRaises(Exception, b.single)
+        self.assertEqual(c.single(), 1)
+        self.assertEqual(a.single(lambda x: x == 3), 3)
+        self.assertRaises(Exception, d.single, lambda x: x["id"] == 1)
+
+    def test_single_or_default(self):
+        self.assertEqual(a.single_or_default(), None)
+        self.assertEqual(b.single_or_default(), None)
+        self.assertEqual(c.single_or_default(), 1)
+        self.assertEqual(a.single_or_default(lambda x: x == 3), 3)
+        self.assertEqual(d.single_or_default(lambda x: x["id"] == 1), None)
+
+    def test_where(self):
+        self.assertEqual(a.where(lambda x: x == 3), List([3]))
+        self.assertEqual(a.where(lambda x: False), List())
+        self.assertEqual(b.where(lambda x: True), List())
+        self.assertEqual(d.where(lambda x: x["id"] == 1), d)
 
 if __name__ == '__main__':
     main()
