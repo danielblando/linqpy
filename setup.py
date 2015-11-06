c384ffcc52ea6a288746a3cbdd73d4c8ab9f7635
Daniel Blando <daniel@blando.com.br>
2015-11-06 19:05:13 -0200

    0.4.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='linqpy',
-    version='0.3.1',
+    version='0.4.0',
     url='https://github.com/danielblando/linqpy',
     license='MIT',
     description='Simple implementation of .NET Linq in python list.',
